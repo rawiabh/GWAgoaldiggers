0e9268a18b651008988bf1a0a8281c9861f3c514
esprit <esprit@PC-ESPRIT>
2016-11-02 21:24:00 +0100

    relation between user and session by salsa

--- GWA.Domaine/Entities/Session.cs
+++ GWA.Domaine/Entities/Session.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +11,9 @@ namespace GWA.Domaine.Entities
    public class Session
     {
         public int Id { get; set; }
-        public string UserName { get; set; }
+        [Key, ForeignKey("User")]
+        public int userId { get; set; }
+
+        public virtual User User { get; set; }
     }
 }
