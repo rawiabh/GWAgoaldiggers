adf9b50d03196c11a59f7fc8a37df906fa04919d
esprit <esprit@PC-ESPRIT>
2016-11-02 20:29:25 +0100

    database version 02

--- GWA.Data/Context/GWAContext.cs
+++ GWA.Data/Context/GWAContext.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Ds.Data.Conventions;
+using GWA.Domaine.Entities;
+using System;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
@@ -10,10 +12,31 @@ namespace GWA.Data.Context
    public class GWAContext :DbContext
     {
         public GWAContext()
-              : base("")
+              : base("GWADB")
         {
 
         }
 
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            
+            modelBuilder.Conventions.Add(new DatetimeConvention());
+            
+            modelBuilder.Conventions.Add(new KeyConvention());
+        }
+
+        DbSet<User> users { get; set; }
+        DbSet<Product> products { get; set; }
+        DbSet<Auction> auctions { get; set; }
+        DbSet<Token> tokens { get; set; }
+        DbSet<Category> category { get; set; }
+        DbSet<Notification> notifications { get; set; }
+        DbSet<Bid> bids { get; set; }
+        DbSet<Payment> payments { get; set; }
+        DbSet<Session> sessions { get; set; }
+        DbSet<ShoppingCart> shoppingCarts { get; set; }
+        //DbSet<Subscription> subscription { get; set; }
+        DbSet<Command> commands { get; set; }
     }
 }
---
+++ GWA.Data/Conventions/KeyConvention.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ds.Data.Conventions
+{
+    public class KeyConvention : Convention
+    {
+        public KeyConvention()
+        {
+
+            this.Properties<int>()
+
+              .Where(p => p.Name.StartsWith("Id")
+                  )
+
+              .Configure(p => p.IsKey());
+        }
+    }
+}
