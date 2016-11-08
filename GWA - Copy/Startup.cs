0258e9d057ee024628fcc111047835c4eccc2dac
nourWag <noureimen.ouaganouni@esprit.tn>
2016-11-08 21:15:28 +0100

    Initial commit of gittattributes and foo.csproj

---
+++ GWA - Copy/Startup.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Owin;
+using MySql.Data.Entity;
+using Owin;
+using System.Data.Entity;
+
+[assembly: OwinStartupAttribute(typeof(GWA.Startup))]
+namespace GWA
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+            DbConfiguration.SetConfiguration(new MySqlEFConfiguration());
+        }
+    }
+}
