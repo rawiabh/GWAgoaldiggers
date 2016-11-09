293dba2c2584a785af2d524e4aa90fa237943425
Nournourf <nour.faroua@esprit.tn>
2016-11-10 00:40:17 +0100

    basic Shopping Cart Code [NoRealTestsPossible]

--- GWA.Domaine/Entities/Buyer.cs
+++ GWA.Domaine/Entities/Buyer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,5 +11,8 @@ namespace GWA.Domaine.Entities
     {
         public String Type { get; set; }
         public String PersonnalDescription { get; set; }
+        public ShoppingCart ShoppingCart { get; set; }
+
+
     }
 }
---
+++ GWA.WEB1/Models/ShoppingCarts/ShoppingCartViewModel.cs
@@ -0,0 +1,11 @@
+﻿using GWA.Domaine.Entities;
+using System.Collections.Generic;
+
+namespace GWA.WEB1.Models.ShoppingCarts
+{
+    public class ShoppingCartViewModel
+    {
+        public List<Command> CartItems { get; set; }
+        public double CartTotal { get; set; }
+    }
+}
