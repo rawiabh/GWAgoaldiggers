c6ac50c0a34bcd6bf8ac9e5c6b72b701a9839a7e
Nournourf <nour.faroua@esprit.tn>
2016-11-10 01:30:41 +0100

    fixed commit
    basic shopping cart code

--- GWA.Domaine/Entities/ShoppingCart.cs
+++ GWA.Domaine/Entities/ShoppingCart.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,7 +10,9 @@ namespace GWA.Domaine.Entities
     public class ShoppingCart
     {
         public int Id { get; set; }
-        public int Quantite { get; set; }
-        public int NbrArticle { get; set; }
+        public int Count { get; set; }
+        // public Buyer Buyer { get; set; }
+        public virtual ICollection<Command> Commands { get; set; }
+
     }
 }
---
+++ GWA.WEB1/Models/ShoppingCart/ShoppingCartViewModel.cs
@@ -0,0 +1,14 @@
+﻿
+
+using GWA.Domaine.Entities;
+using System.Collections.Generic;
+
+namespace GWA.WEB1.Models.ShoppingCarts
+{
+    public class ShoppingCartViewModel
+    {
+        public List<Command> CartItems { get; set; }
+        public double CartTotal { get; set; }
+    }
+}
+
